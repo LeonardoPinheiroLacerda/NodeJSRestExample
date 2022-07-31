155d77a23604177fd92d2c93c619b05d0096d89f
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-07-31 18:49:25 -0300

    Listagem única e validação

--- index.js
+++ index.js
@@ -36,6 +36,34 @@ app.get('/games', (req, res) => {
         .json(DB.games);
 });
 
+app.get('/games/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(isNaN(id)){
+        res
+            .status(400)
+            .json(
+                {
+                    status: 400,
+                    message: "ID must have a numeric value"
+                }
+            );
+        return;
+    }
+
+    const game = DB.games.find(game => game.id === parseInt(id));
+
+    if(game){
+        res 
+            .status(200)
+            .json(game);
+    }else{
+        res
+            .status(404)
+            .send();
+    }
+})
+
 app.listen(8080, () => {
     console.log("API running on port 8080");
 })
