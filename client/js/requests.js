bbb2ca1425da6bb731161a8928d94b456789d602
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-08-01 17:40:12 -0300

    Atualizando registros de jogos

--- client/js/requests.js
+++ client/js/requests.js
@@ -2,6 +2,7 @@ const END_POINT = "http://localhost:8080";
 
 const TBODY = document.querySelector("#game-list");
 const CREATION_FORM = document.querySelector("#new-game");
+const UPDATE_FORM = document.querySelector("#edit-game");
 
 async function getAllGames(){
     return axios.get(`${END_POINT}/games`);
@@ -22,7 +23,7 @@ async function updateGameTable(){
                     <td>${game.year}</td>
                     <td class="col-lg-3 col-md-4 col-sm-5">
                         <button class="btn btn-warning mx-1"
-                            onclick="fillUpdateFields(${game.id}">
+                            onclick="loadUpdateFields(${game.id})">
                             Atualizar
                         </button>
                         <button class="btn btn-danger mx-1" 
@@ -36,6 +37,7 @@ async function updateGameTable(){
 
         TBODY.innerHTML = trs;
 
+        scrollTo(0, 0);
     }catch(e){
         alert(`Algo deu errado: ${e}.`);
     }
@@ -51,6 +53,27 @@ function deleteItem(id){
     });
 }
 
+async function loadUpdateFields(id){
+    const getById = async () => {
+        return axios(`${END_POINT}/games/${id}`);
+    }
+
+    try{
+        const response = await getById();
+        const game = response.data;
+
+        document.querySelector("#edit-game-id").value = game.id;
+        document.querySelector("#edit-game-title").value = game.title;
+        document.querySelector("#edit-game-price").value = game.price;
+        document.querySelector("#edit-game-year").value = game.year;
+
+        scrollTo(0, document.body.scrollHeight);
+        
+    }catch(e){
+        alert(`Algo deu errado: ${e}.`);
+    }
+}
+
 CREATION_FORM.addEventListener("submit", (evt) => {
     evt.preventDefault();
 
@@ -62,6 +85,25 @@ CREATION_FORM.addEventListener("submit", (evt) => {
 
     axios.post(`${END_POINT}/games`, game).then(res => {
         if(res.status == 201){
+            updateGameTable();
+        }
+    }).catch(e => {
+        alert(`Algo deu errado: ${e}.`);
+    });
+});
+
+UPDATE_FORM.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const id = document.querySelector("#edit-game-id").value;
+    const title = document.querySelector("#edit-game-title").value;
+    const price = document.querySelector("#edit-game-price").value;
+    const year = document.querySelector("#edit-game-year").value;
+
+    const game = {title, price, year};
+
+    axios.put(`${END_POINT}/games/${id}`, game).then(res => {
+        if(res.status == 200){
             updateGameTable();
         }
     }).catch(e => {
